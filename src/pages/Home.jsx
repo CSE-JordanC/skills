ffa23a727331831e514415684e33353d7da33b6e
CSE-JordanC <chavousn@email.sc.edu>
2026-03-17 09:11:19 -0400

    homepage setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import "../css/Home.css";
+
+const Home = () => {
+    return (
+        <main id="home" class="main-content">
+            <h2>Home</h2>
+            <p>I wil display on the home page</p>
+        </main>
+    );
+};
+
+export default Home;
